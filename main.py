5b1dbb754cc224a9d5a4da94af019fb759b01bff
Codeissimple <adam.jkirjacev@gmail.com>
2019-02-21 05:32:17 +0000

    New webpages, 2 out of 3 pages added. CSS, JS. added

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route("/about")
-def huxley():
-    return render_template('about.html')
+@app.route("/elements")
+
+def elements():
+    return render_template('elements.html')
+
+@app.route("/generic")
+
+def generic():
+    return render_template('generic.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
